2a8a98f1fe55b2959234fd4cc259e32d2aafba20
med1na <medina_79@outlook.com>
2023-01-31 23:01:53 -0300

    se crearon componentes y se realizo el fraccionamiento de la pagina

---
+++ src/app/componentes/laboral/laboral.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laboral',
+  templateUrl: './laboral.component.html',
+  styleUrls: ['./laboral.component.css']
+})
+export class LaboralComponent {
+
+}
